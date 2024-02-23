ebde853fd724ce3c151951bcafc661f386e0775d
woodsman22 <woodsman22@woodsman22.com>
2024-02-23 16:33:40 -0500

    work on exercise61

--- Python/exercises/exercise61.py
+++ Python/exercises/exercise61.py
@@ -10,17 +10,34 @@
 import string
 alphabet_list = list(string.ascii_lowercase)
 
-def print_line(index):
-    print('-'.join(alphabet_list[index:]))
+def print_line(index,size):
+    forward_list = alphabet_list[index:size]
+    reverse_list = alphabet_list[index:size][::-1]
+    reverse_list.pop()
+
+    #  Creating dashes list
+    number_of_dashes = int(((index*2) / 2))
+    string_of_dashes = '-' * number_of_dashes
+
+    print(string_of_dashes + '-'.join(reverse_list + forward_list) + \
+          string_of_dashes)
+
+    if number_of_dashes == 0:
+        pass
+    else:
+        line_without_dashes = '-'.join(reverse_list + forward_list)
+        # print(line_without_dashes)
+        # print(string_of_dashes +  line_without_dashes + string_of_dashes)
+        # if index == 0 dashes * 2 
+        # if index == 1 dashes * 2 = --
+        # if index == 2 dashes * 2 = ----
+        # if index == 3 dashes * 2 = ------
+        # if index == 5 dashes * 2 = ----------
+        # if index == 7 dashes * 2 = --------------
 
 def print_rangoli(size):
-    print(alphabet_list[0:size])
     for index in range(0,len(alphabet_list[0:size])):
-        print_line(index)
-        # your code goes here
-        pass
+        print_line(index,size)
 
 if __name__ == '__main__':
     print_rangoli(5)
-    # n = int(input())
-    # print_rangoli(n)
