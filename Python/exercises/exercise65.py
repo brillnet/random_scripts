3d1e71c09875fadd93995e2fcc8832ffd68ac9f0
woodsman22 <woodsman22@woodsman22.com>
2024-03-12 11:21:53 -0400

    more work on exercise65

--- Python/exercises/exercise65.py
+++ Python/exercises/exercise65.py
@@ -1,30 +1,35 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
-def check_list(count,initial_list,comparison_list,list_type):
-    for val in initial_list:
-        #  If list_type is 'a' and val is in comparison list
-        #  a point is added to the total count.
-        if list_type == 'a':
-            if val in comparison_list:
-                count = count + 1
-        else:
+def check_set(count,initial_set,comparison_set,set_type):
+
+    #  If list_type is 'a' and val is in comparison list
+    #  a point is added to the total count.
+    if set_type == 'a':
+
+        #  Getting the total number of intersections
+        count = len(initial_set.intersection(comparison_set))
+        return count
+    else:
         #  If list_type is 'b' and val is in comparison list
         #  a point is added to the total count.
-            if val in comparison_list:
-                count = count - 1
-
+        length_of_negatives = len(initial_set.intersection(comparison_set))
+        count = count - length_of_negatives
     return count
     
 #  This counter will keep track of the score.       
 count = 0
 
-n_and_m_list = sys.stdin.readline().strip().split()
-initial_list = sys.stdin.readline().strip().split()
-a_list = sys.stdin.readline().strip().split()
-b_list = sys.stdin.readline().strip().split()
+n_and_m_set = sys.stdin.readline().strip().split()
+initial_set = sys.stdin.readline().strip().split()
+a_set = sys.stdin.readline().strip().split()
+b_set = sys.stdin.readline().strip().split()
 
-count = check_list(count,initial_list,a_list,'a')
-count = check_list(count,initial_list,b_list,'b')
+
+# zip the two lists together to create a list of key-value pairs
+key_value_pairs = zip(keys, values)
+
+count = check_set(count,initial_set,a_set,'a')
+count = check_set(count,initial_set,b_set,'b')
 
 print(count)
