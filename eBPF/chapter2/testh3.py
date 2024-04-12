bcc8106a38f55b164abc3044e89994e12ec9d9ea
none <none@none.com>
2024-04-11 21:59:01 -0400

    eBPF. Need to add comments to test4h.py

--- eBPF/chapter2/testh3.py
+++ eBPF/chapter2/testh3.py
@@ -5,7 +5,8 @@ from time import sleep
 #  The below C program creates a global data structure
 #  called output that will be passeed up to the userspace
 #  python script. This data structure will consist of
-#  three pieces of data. The first is ctx the second is
+#  three variables. The first is ctx, this is for passing of
+#  context from the calling eBPG program to the calle the second is
 #  data, this is a struct that holds a pid,uid,command executed
 #  and the message.  The next is the total size of the data
 #  structure. 
@@ -51,12 +52,17 @@ syscall = b.get_syscall_fnname("execve")
 #  in the kernel.
 b.attach_kprobe(event=syscall, fn_name="hello")
 
-
+#  Function to print data sent from C program.
 def print_event(cpu, data, size):
     data = b["output"].event(data)
     print(f"{data.pid} {data.uid} {data.command.decode()} " + \
     f"{data.message.decode()}")
 
+#  Open perf ring buffer (memory) and assigns the
+#  print_event function as a call back function
+#  everytime data is read from the buffer.
 b["output"].open_perf_buffer(print_event)
+
+#  Reading data forever.
 while True:
     b.perf_buffer_poll()
