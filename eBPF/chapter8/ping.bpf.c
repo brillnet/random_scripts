d042fae2acdbeaee7d11104093c37a43f86a7a01
none <none@none.com>
2024-09-10 01:33:38 +0000

    more changes

--- eBPF/chapter8/ping.bpf.c
+++ eBPF/chapter8/ping.bpf.c
@@ -2,12 +2,21 @@
 #include <bcc/proto.h>
 #include <linux/pkt_cls.h>
 
+
 int xdp(struct xdp_md *ctx) {
   void *data = (void *)(long)ctx->data;
   void *data_end = (void *)(long)ctx->data_end;
 
   if (is_icmp_ping_request(data, data_end)) {
         bpf_trace_printk("Got ping packet");
+        return XDP_PASS;
+  }
+
+  // unsigned short a is_https_traffic(data, data_end);
+  bpf_trace_printk("Port: %u\n", is_https_traffic(data, data_end));
+
+  if (is_https_traffic(data, data_end)) {
+        bpf_trace_printk("Got https packet!");
         return XDP_PASS;
   }
 
