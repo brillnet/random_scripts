e9955de9574d02db48f376fa3d240c522cb54681
none <none@none.com>
2024-10-14 22:32:17 +0000

    c - exercise23

---
+++ c/exercise23.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    char alphabet[26];
+    int x;
+    char *pn;
+
+    // Intializing pointer
+    pn = alphabet;
+
+    // Filling array
+    for(x=0;x<26;x++)
+    {
+        //  Incremnting the value at pointer memory address 
+        //  adding x to char 'A' doing this 26 times. This 
+        // should get us every letter in the alphabet.
+
+        //   Incrementing the memory pointer by FOUR bytes
+        //   Remember when incrementing a pointer it will 
+        //   increment by the default size of the data type
+        //   set for the pointer.
+        *pn++=x+'A';
+
+    }
+
+    // The below line is odd to me because if you print the memory
+    // address of pn with %pn before the pointer is reinitialized
+    // it will be the same as after reinitializing.
+
+    // Re-Intializing pointer. 
+    pn = alphabet;
+
+
+    //  Filling and printing values in array and memory addresses for each
+    //  variable.
+    for(x=0;x<26;x++)
+    {
+        
+        //  Printing value at pointers memory location in array.
+        putchar(*pn);
+
+        // Incrementing pointer
+        pn++;
+    }
+    putchar('\n');
+
+    return(0);
+
+}
